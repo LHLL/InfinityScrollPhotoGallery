b9a83de80f6804b5cf636a1def5d38b159fc7596
Jay Xu <Lynn@lynns-macbook-pro.local>
2016-05-01 14:07:14 -0700

    Initial Commit

---
+++ InfinityScrollPhotoGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InfinityScrollPhotoGallery
+//
+//  Created by 李玲 on 5/1/16.
+//  Copyright © 2016 Jay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
